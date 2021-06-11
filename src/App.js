facb5b5d0cb76c842f6ac81bc863f7f384429d8a
thuccv98 <thuccv98@gmail.com>
2021-06-11 11:45:22 +0700

    commit header and footer component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer.js";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Trending from "./Pages/Trending/Trending.js";
+import Movies from "./Pages/Movies/Movies.js";
+import Series from "./Pages/Series/Series.js";
+import Search from "./Pages/Search/Search.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <container>
+          <Switch>
+            <Route path='/' component={Trending} exact/>
+            <Route path='/movies' component={Movies} />
+            <Route path='/series' component={Series} />
+            <Route path='/search' component={Search} />
+          </Switch>
+        </container>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return <span className="header">🎥Movie Wep App🎬</span>
+};
+
+export default Header;
